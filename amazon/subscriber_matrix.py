a7adbdb3bca4079439a4146efa281a987c5e7d12
shailesh-shenoy <shenoy.shailesh97@gmail.com>
2024-05-29 07:58:43 -0400

    Union Find

--- amazon/subscriber_matrix.py
+++ amazon/subscriber_matrix.py
@@ -1,21 +1,52 @@
 from typing import List
 
 
-def find_sub_groups(sub_mat: List[str]) -> int:
-
+def find_sub_groups(subs: List[str]) -> int:
+    if not subs:
+        return 0
+    n = len(subs)
     mat = []
-    allowed = set(["0", "1"])
-    for i, s in enumerate(sub_mat):
+    allowed = set(["1", "0"])
+    for s in subs:
+        if len(s) != n:
+            continue
         row = []
-        for j, c in enumerate(s):
-            if c not in allowed:
+        for c in s:
+            if c != "1" and c != "0":
                 break
             row.append(c)
-        if len(row) == len(s):
+        if len(row) == n:
             mat.append(row)
 
-    return len(sub_mat)
+    if len(mat) != n:
+        return 0
+
+    parent = [i for i in range(n)]
+
+    count = n
+
+    def find(x):
+
+        while parent[x] != x:
+            x = parent[x]
+        return x
+
+    def union(x, y):
+        nonlocal count
+        p_x = find(x)
+        p_y = find(y)
+        if p_x == p_y:
+            return
+        parent[p_y] = p_x
+        count -= 1
+
+    for i in range(n):
+        for j in range(n):
+            if mat[i][j] == "1":
+                union(i, j)
+
+    return count
 
 
 if __name__ == "__main__":
-    print(find_sub_groups(["110", "110", "001"]))
+    print(find_sub_groups(["100", "010", "001"]))
